f2995e08cd4338ec786c2db948e32e66ef7bf917
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-04-02 14:46:32 -0400

    misunderstood goal - changing course

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -13,18 +13,29 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
         int tempCount;
+        int arrayLengthCounter = 0;
         Integer mostFreq = null;
         for (int i = 0; i < array.length -1; i++) {
             tempCount = 0;
             for (int j = 0; j < array.length; j++) { //compares current index and adds to tempCount
-                if(array[i] == array[j]){
+                if(array[i].equals(array[j])){
                     tempCount++;
                 }
             }
-            if(tempCount == maxNumberOfDuplications){
-                mostFreq = array[i]; //change mostFreq to current index
+            if(tempCount >= maxNumberOfDuplications){
+                for (int j = i+1; j < array.length; j++) {
+                    array[j] = null;
+                }
+              //  mostFreq = array[i]; //change mostFreq to current index
             }
         }
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] != null){
+                arrayLengthCounter += 1;
+            }
+        }
+        Integer[] arr = new Integer[arrayLengthCounter];
+        
         return new Integer[0];
     }
 
