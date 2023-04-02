53df46b22634140349d74d0585bee4ba688d384f
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-04-02 15:42:39 -0400

    progress maybe

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -12,31 +12,37 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        IntegerDuplicateDeleter deleter = new IntegerDuplicateDeleter(this.array);
         int tempCount;
-        int arrayLengthCounter = 0;
+        int arraySubtractCounter = 1;
         Integer mostFreq = null;
-        for (int i = 0; i < array.length -1; i++) {
+        Integer checker;
+        for (int i = 0; i < deleter.array.length -1; i++) {
             tempCount = 0;
-            for (int j = 0; j < array.length; j++) { //compares current index and adds to tempCount
-                if(array[i].equals(array[j])){
+            for (int j = i+1; j < deleter.array.length; j++) { //compares current index and adds to tempCount
+                if(this.array[i].equals(deleter.array[j])){
                     tempCount++;
                 }
             }
-            if(tempCount >= maxNumberOfDuplications){
-                for (int j = i+1; j < array.length; j++) {
-                    array[j] = null;
+            if(tempCount >= maxNumberOfDuplications){ //null index if repeats is at least max
+                for (int j = i+1; j < deleter.array.length; j++) {
+                    if (deleter.array[i].equals(array[j])) {
+                        deleter.array[j] = null;
+                    }
                 }
+                arraySubtractCounter += tempCount;
               //  mostFreq = array[i]; //change mostFreq to current index
             }
         }
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] != null){
-                arrayLengthCounter += 1;
+        Integer[] arr = new Integer[deleter.array.length - arraySubtractCounter];
+        int arrSpot = 0;
+        for (int i = 0; i < deleter.array.length; i++) {
+            if (deleter.array[i] != null){
+                arr[arrSpot] = deleter.array[i];
+                arrSpot++;
             }
         }
-        Integer[] arr = new Integer[arrayLengthCounter];
-        
-        return new Integer[0];
+        return arr;
     }
 
     @Override
